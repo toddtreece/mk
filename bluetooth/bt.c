29207e104f5aa0fd1735f4c5494dc32a181440b4
Todd Treece <toddtreece@gmail.com>
2011-12-28 16:02:00 -0700

    initialize timer0

--- bluetooth/bt.c
+++ bluetooth/bt.c
@@ -1,5 +1,6 @@
 #define F_CPU 16000000UL
 #define RX_STARVE 20
+#define KEY_REFRESH_RATE 15
 
 #include <inttypes.h>
 #include <util/delay.h>
@@ -10,68 +11,73 @@
 volatile uint8_t scan_keypads;
 
 ISR(TIMER0_COMPA_vect) {
-	scan_keypads = 1;
-	TCNT0 = 0;
+  scan_keypads = 1;
+  TCNT0 = 0;
 }
 
 void USARTInit(uint16_t ubrr_value) {
 
-   //Set Baud rate
-   UBRRL = ubrr_value;
-   UBRRH = (ubrr_value>>8);
+  //Set Baud rate
+  UBRRL = ubrr_value;
+  UBRRH = (ubrr_value>>8);
 
-   UCSRC=(1<<USBS)|(3<<UCSZ0);
-   UCSRB=(1<<RXEN)|(1<<TXEN);
+  UCSRC=(1<<USBS)|(3<<UCSZ0);
+  UCSRB=(1<<RXEN)|(1<<TXEN);
 
 }
 
 void main() {
-	
-	// 115200 baud
-	USARTInit(8);
-	
-	uint8_t starve;
 
-	DDRB = 0;
-	PORTB = 0;
-	
-	sei();
-	
-	while(1) {
+  // 115200 baud
+  USARTInit(8);
 
-		starve = 0;
+  uint8_t starve;
 
-		//output
-		PORTB = 0;
-		DDRB = 0xFF;
-		
-		while((UCSRA & (1<<RXC)) && starve < RX_STARVE) {
-			starve++;
-			
-			PORTB = UDR;
-		}
-		
-		if(scan_keypads) {
-			
-			scan_keypads = 0;
-			
-			//input
-			PORTB = 0;
-			DDRB = 0;
-			
-			_delay_us(1);
-			
-			//wait until ready to send
-			while(!(UCSRA & (1<<UDRE))) {}
+  DDRB = 0;
+  PORTB = 0;
 
-			UDR = PORTB;
-		}
+  // keypad timer init
+  TCCR0B |= (1<<CS02) | (1<<CS00);
+  TIMSK |= (1 << OCIE0A);
+  OCR0A = KEY_REFRESH_RATE;
 
-	}
-	
-	return;
-	
+  sei();
+
+  while(1) {
+
+    starve = 0;
+
+    //output
+    PORTB = 0;
+    DDRB = 0xFF;
+
+    while((UCSRA & (1<<RXC)) && starve < RX_STARVE) {
+      starve++;
+
+      PORTB = UDR;
+    }
+
+    if(scan_keypads) {
+
+      scan_keypads = 0;
+
+      //input
+      PORTB = 0;
+      DDRB = 0;
+
+      _delay_us(1);
+
+      //wait until ready to send
+      while(!(UCSRA & (1<<UDRE))) {}
+
+      UDR = PORTB;
+    }
+
+  }
+
+  return;
+
 }
 
-	
-	
+
+
